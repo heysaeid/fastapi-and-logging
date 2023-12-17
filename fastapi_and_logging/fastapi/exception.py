e9305d73fa2a435aea333e7865a80e6ed7e8fedc
heysaeid <heysaeid92@gmail.com>
2023-12-17 20:40:12 +0330

    🎨 Rename error logger with exception logger

--- fastapi_and_logging/fastapi/exception.py
+++ fastapi_and_logging/fastapi/exception.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from fastapi_and_logging.enums import LogTypeEnum
-from fastapi_and_logging.logging import get_error_logger
+from fastapi_and_logging.logging import get_exception_logger
 
 
-def get_error_logger_default_values(request: Request) -> dict:
+def get_exception_logger_default_values(request: Request) -> dict:
     scope = request.scope
     return {
         "request_id": request.state.request_id,
@@ -30,12 +30,12 @@ class ExceptionLogger:
         request: Request, 
         exc: Exception,
     ):
-        get_error_logger(
+        get_exception_logger(
             file_path = self.log_path,
             log_type = self.log_type,
             extra_data = {
                 "error_message": str(exc),
-                **get_error_logger_default_values(request),
+                **get_exception_logger_default_values(request),
             }
         )
         return JSONResponse(
--- fastapi_and_logging/logging.py
+++ fastapi_and_logging/logging.py
@@ -55,21 +55,21 @@ def get_apicall_logger(
     elif log_type == LogTypeEnum.CONSOLE:
         logger.info({"message":message, "data":extra_data})
         
-def error_formatter(record: dict):
+def exception_formatter(record: dict):
     record["extra"]["serialized"] = json.dumps(record["extra"]["data"], default=str)
     return "{extra[serialized]}\n"
 
-def get_error_logger(
-    file_path: str = "error.log", 
+def get_exception_logger(
+    file_path: str = "exception.log", 
     enqueue: bool = True, 
     extra_data: dict = {},
-    message: str = "Error",
-    format = error_formatter,
+    message: str = "Exception",
+    format = exception_formatter,
     log_type: LogTypeEnum = LogTypeEnum.FILE,
 ):
     if log_type == LogTypeEnum.FILE:     
         error_logger = create_logger(
-            name = "error",
+            name = "exception.log",
             file_path = file_path,
             enqueue = enqueue,
             format = format,
